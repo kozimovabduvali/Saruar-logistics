842a59567a6bf4f0715e8f8f83b6a35d57c2ed68
kozimovabduvali <kozimovabduvali2006@gmail.com>
2025-02-20 13:00:21 +0500

    map

--- public/js/script.js
+++ public/js/script.js
@@ -45,3 +45,75 @@ let productSwiper = new Swiper(".product-swiper__1", {
     },
   }
 }); 
+
+// Swiper 2
+let swiperSponsors = new Swiper(".swiper__sponsors", {
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    640: {
+      slidesPerView: 2.5,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 5.5,
+      spaceBetween: 20,
+    },
+    1280: {
+      slidesPerView: 7,
+      spaceBetween: 20,
+    },
+    1650: {
+      slidesPerView: 8,
+      spaceBetween: 22,
+    },
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+}); 
+
+// Swiper 3
+let swiperAdvantages = new Swiper(".swiper__advantages", {
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1.3,
+      spaceBetween: 30,
+    },
+    640: {
+      slidesPerView: 1.7,
+      spaceBetween: 30,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 35,
+    },
+    1280: {
+      slidesPerView: 3.5,
+      spaceBetween: 40,
+    },
+    1500: {
+      slidesPerView: 4,
+      spaceBetween: 45,
+    },
+  },
+}); 
